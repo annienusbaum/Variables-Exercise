289294c6f58a04db5d2c70f6e9bcf4aae68b07e4
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-11-03 12:12:09 -0400

    VariablesExercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string name = "Annie";
+            int age = 31;
+            char middleInitial = 'J';
+            bool skyIsBlue = true;
+            double randomNumber = 55.5;
+            decimal randomDecimal = 77.77m;
+            Console.WriteLine($"My name is {name}, I am {age}. My least favorite number is {randomDecimal} always");
+
+}
+
     }
 }
